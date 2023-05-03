6579c4d8db18dba5d3a14abe79fc1439f1193036
Zifeng Luo <87281041+luozf14@users.noreply.github.com>
2023-05-03 13:59:21 -0500

    Add stage2

--- runOnCluster.py
+++ runOnCluster.py
@@ -2,14 +2,21 @@ from pyspark import StorageLevel, SparkFiles, SparkContext, SparkConf
 import subprocess,time,json
 
 def run_sim(index) :
+  # Stage1
   with open("config.json") as infile :
     config = json.load(infile)
   config["ProcessNumber"] = index
-  with open("config_{0}.json".format(index),"w") as outfile :
+  with open("config_stage1~{0}.json".format(index),"w") as outfile :
     json.dump(config,outfile,indent = 4)
-  subprocess.call(["./MdmPpacSim","config_{0}.json".format(index)])
-#   subprocess.call(["mv","Stage1~{0}.root".format(index),"/hdfs/user/luozf/MdmPpacSimResults/"])
-  subprocess.call(["hdfs", "dfs", "-moveFromLocal", "-f", "Stage1~{0}.root".format(index), "/user/luozf/MdmPpacSimResults"])
+  subprocess.call(["./MdmPpacSim","config_stage1~{0}.json".format(index)])
+
+  # Stage2
+  config["IsTargetChamber"] = "false"
+  with open("config_stage2~{0}.json".format(index),"w") as outfile :
+    json.dump(config,outfile,indent = 4)
+  subprocess.call(["./MdmPpacSim","config_stage2~{0}.json".format(index)])
+
+#   subprocess.call(["hdfs", "dfs", "-moveFromLocal", "-f", "Stage1~{0}.root".format(index), "/user/luozf/MdmPpacSimResults"])
 
 if __name__ == "__main__" :
   sconf = SparkConf().setAppName("MdmPpacSim")
