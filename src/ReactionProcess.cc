c43fdefc2a97398d9654635ef0c549391ed147ef
luozf14 <87281041+luozf14@users.noreply.github.com>
2023-08-13 14:23:38 -0500

    update

--- src/ReactionProcess.cc
+++ src/ReactionProcess.cc
@@ -50,26 +50,26 @@ namespace MdmPpacSim
         //   TwoBody(aTrack,aStep,fTargetCharge, fTargetMass,fLightProductCharge, fLightProductMass, fHeavyProductCharge, fHeavyProductMass, 0.0, 4.44 );
 
         G4double ran = G4UniformRand();
-        if (ran < 0.2)
-        {
-            TwoBodyDecay(aTrack, aStep, fTargetCharge, fTargetMass, fLightProductCharge, fLightProductMass, 0.0, fHeavyProductCharge, fHeavyProductMass, 4.44, 1);
-        }
-        else if (ran < 0.4)
+        if (ran < 0.26)
         {
             TwoBodyDecay(aTrack, aStep, fTargetCharge, fTargetMass, fLightProductCharge, fLightProductMass, 0.0, fHeavyProductCharge, fHeavyProductMass, 7.65, 1);
         }
-        else if (ran < 0.6)
-        {
-            TwoBodyAlphaDecay(aTrack, aStep, 8, 16, 2, 4, 0.0, 8, 16, 10.36);
-        }
-        else if (ran < 0.8)
-        {
-            TwoBodyNeutronDecay(aTrack, aStep, 6, 13, 2, 4, 0.0, 6, 13, 6.864);
-        }
         else
         {
-            TwoBodyNeutronDecay(aTrack, aStep, 6, 13, 2, 4, 0.0, 6, 13, 7.55);
+            TwoBodyDecay(aTrack, aStep, fTargetCharge, fTargetMass, fLightProductCharge, fLightProductMass, 0.0, fHeavyProductCharge, fHeavyProductMass, 7.65, 2);
         }
+        // else if (ran < 0.6)
+        // {
+        //     TwoBodyAlphaDecay(aTrack, aStep, 8, 16, 2, 4, 0.0, 8, 16, 10.36);
+        // }
+        // else if (ran < 0.8)
+        // {
+        //     TwoBodyNeutronDecay(aTrack, aStep, 6, 13, 2, 4, 0.0, 6, 13, 6.864);
+        // }
+        // else
+        // {
+        //     TwoBodyNeutronDecay(aTrack, aStep, 6, 13, 2, 4, 0.0, 6, 13, 7.55);
+        // }
         return &aParticleChange;
     }
 
