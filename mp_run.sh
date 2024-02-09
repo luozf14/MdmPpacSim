443fe5e63e08078854359c6c01ed31c245bb30a4
luozf14 <87281041+luozf14@users.noreply.github.com>
2024-02-09 16:56:23 +0800

    add automatic processing for stage2

--- mp_run.sh
+++ mp_run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-config="../config/config.json"
 num_processes=3
 
 # Function to handle termination signal
@@ -14,7 +13,7 @@ function terminate_script() {
 trap terminate_script SIGINT SIGTERM
 
 rm log_*.txt
-rm Stage1.root
+rm *.root
 
 # Start time for the script
 start_time=$(date +%s.%N)
@@ -22,17 +21,29 @@ start_time=$(date +%s.%N)
 for ((process_num=0; process_num<num_processes; process_num++))
 do
   (
-    # Start time for the MdmPpacSim process
-    mdm_start_time=$(date +%s.%N)
+    # Start time for the MdmPpacSim Stage 1 process
+    stage1_start_time=$(date +%s.%N)
     
-    ./MdmPpacSim "$config" "$process_num" > "log_$process_num.txt"
+    ./MdmPpacSim ../config/stage1.json "$process_num" > "log_stage1_$process_num.txt"
     
-    # End time for the MdmPpacSim process
-    mdm_end_time=$(date +%s.%N)
+    # End time for the MdmPpacSim Stage 1 process
+    stage1_end_time=$(date +%s.%N)
     
-    mdm_duration=$(awk "BEGIN { printf \"%.2f\", ($mdm_end_time - $mdm_start_time) / 60 }")
+    stage1_duration=$(awk "BEGIN { mins=int(($stage1_end_time - $stage1_start_time) / 60); secs=($stage1_end_time - $stage1_start_time) % 60; printf \"%d min %d sec\", mins, secs }")
     
-    echo "Time taken for MdmPpacSim process $process_num: $mdm_duration minutes" >> "log_$process_num.txt"
+    echo "Time taken for MdmPpacSim Stage 1 process $process_num: $stage1_duration" >> "log_stage1_$process_num.txt"
+    
+    # Start time for the MdmPpacSim Stage 2 process
+    stage2_start_time=$(date +%s.%N)
+    
+    ./MdmPpacSim ../config/stage2.json "$process_num" > "log_stage2_$process_num.txt"
+    
+    # End time for the MdmPpacSim Stage 2 process
+    stage2_end_time=$(date +%s.%N)
+    
+    stage2_duration=$(awk "BEGIN { mins=int(($stage2_end_time - $stage2_start_time) / 60); secs=($stage2_end_time - $stage2_start_time) % 60; printf \"%d min %d sec\", mins, secs }")
+    
+    echo "Time taken for MdmPpacSim Stage 2 process $process_num: $stage2_duration" >> "log_stage2_$process_num.txt"    
   ) &
 done
 
@@ -42,20 +53,22 @@ wait
 # Start time for the hadd command
 hadd_start_time=$(date +%s.%N)
 
-# Merge the root files using hadd
-hadd -f Stage1.root Stage1_*.root
+# Merge the Stage 1 root files
+hadd Stage1.root Stage1_*.root
+rm Stage1_*.root
+
+# Merge the Stage 2 root files
+hadd Stage2.root Stage2_*.root
+rm Stage2_*.root
 
 # End time for the hadd command
 hadd_end_time=$(date +%s.%N)
 
-hadd_duration=$(awk "BEGIN { printf \"%.2f\", ($hadd_end_time - $hadd_start_time) / 60 }")
-
-# Remove individual root files
-rm Stage1_*.root
+hadd_duration=$(awk "BEGIN { mins=int(($hadd_end_time - $hadd_start_time) / 60); secs=($hadd_end_time - $hadd_start_time) % 60; printf \"%d min %d sec\", mins, secs }")
 
 # Total time taken for the script
 end_time=$(date +%s.%N)
-total_time=$(awk "BEGIN { printf \"%.2f\", ($end_time - $start_time) / 60 }")
+total_duration=$(awk "BEGIN { mins=int(($end_time - $start_time) / 60); secs=($end_time - $start_time) % 60; printf \"%d min %d sec\", mins, secs }")
 
-echo "Time taken for MdmPpacSim processes: $total_time minutes"
-echo "Time taken for hadd command: $hadd_duration minutes"
+echo "Time taken for MdmPpacSim Stage 1 and Stage 2 processes: $total_duration"
+echo "Time taken for hadd command: $hadd_duration"
